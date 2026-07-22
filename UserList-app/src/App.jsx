50a1a70e50d1d808d5b0eccdfddb418eef741ebf
Pawan kumar Yadav <pawan902670@gmail.com>
2026-07-22 18:07:04 +0530

    Adding new Project (Help to understand how can fetch API )

--- UserList-app/src/App.jsx
+++ UserList-app/src/App.jsx
@@ -3,9 +3,16 @@ import "./App.css";
 
 export default function App() {
   const [users, setUsers] = useState([]);
+
+  const [search, setSearch] = useState("");
+
+
   const [loading, setLoading] = useState(false);
+
+
   const [error, setError] = useState("");
 
+ 
   const getUserData = async () => {
     try {
       setLoading(true);
@@ -29,25 +36,47 @@ export default function App() {
     }
   };
 
+  // Call API when component loads
   useEffect(() => {
     getUserData();
   }, []);
 
+  // Filter Users
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="container">
-      <h1>User List</h1>
+      <h1>User Search App</h1>
+
+      <input
+        type="text"
+        placeholder="Search user..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
       {loading && <h2>Loading...</h2>}
 
       {error && <p className="error">{error}</p>}
 
       <div className="users">
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <div className="card" key={user.id}>
             <h2>{user.name}</h2>
-            <p><strong>Email:</strong> {user.email}</p>
-            <p><strong>Phone:</strong> {user.phone}</p>
-            <p><strong>Website:</strong> {user.website}</p>
+
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+
+            <p>
+              <strong>Phone:</strong> {user.phone}
+            </p>
+
+            <p>
+              <strong>Website:</strong> {user.website}
+            </p>
           </div>
         ))}
       </div>
